6fe41d73952088dac83cb0c6084f29543f093641
JanicaJensen <122334098+JanicaJensen@users.noreply.github.com>
2023-06-08 18:33:56 -0600

    added a node package for dynamic inport ?

--- server.js
+++ server.js
@@ -56,40 +56,129 @@ function prompt() {
                 "Add role",
                 "End"]
         })
+        .then(function ({ task }) {
+            switch (task) {
+                case "Show employees":
+                    showEmployees();
+                    break;
+
+                case "Show employees by department":
+                    showByDepartment();
+                    break;
+
+                case "Add an employee":
+                    addEmployee();
+                    break;
+
+                case "Remove an employee":
+                    removeEmployee();
+                    break;
+
+                case "Update employee role":
+                    updateEmployeeRole();
+                    break;
+
+                case "End":
+                    connection.end();
+                    break;
+            }
+        });
 };
-.then(function ({ task }) {
-    switch (task) {
-        case "Show employees":
-            showEmployees();
-            break;
-
-        case "Show employees by department":
-            showByDepartment();
-            break;
-
-        case "Add an employee":
-            addEmployee();
-            break;
-
-        case "Remove an employee":
-            removeEmployee();
-            break;
-
-        case "Update employee role":
-            updateEmployeeRole();
-            break;
-
-        case "End":
-            connection.end();
-            break;
-    }
-});
 
 
 
 
-showEmployees();
-showByDepartment();
-addEmployee();
-removeEmployee();
-updateEmployeeRole();
+
+
+
+
+
+function showEmployees() {
+    console.log('Showing employees\n');
+    connection.query('SELECT * FROM employee', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        prompt();
+    });
+}
+
+
+function showByDepartment() {
+    console.log('Showing employees by department\n');
+    connection.query('SELECT * FROM department', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        prompt();
+    });
+}
+
+function addEmployee() {
+    console.log('Adding employee\n');
+    var query =
+        `
+    SELECT r.id, r.title, r.salary FROM role r
+    `
+
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+
+        const roleChoices = res.map(({ id, title, salary }) => ({
+            value: id, title: `${title}`, salary: `${salary}`
+        }));
+
+        console.table(res);
+        console.log('RoleToInsert!\n');
+
+        promptInsert(roleChoices);
+    });
+}
+
+function promptInsert(roleChoices) {
+
+    inquirer
+        .prompt(
+            [
+                {
+                    type: 'input',
+                    name: 'first_name',
+                    message: 'What is the employees first name?',
+                },
+                {
+                    type: 'input',
+                    name: 'last_name',
+                    message: 'What is the employees last name?',
+                },
+                {
+                    type: 'list',
+                    name: 'role_id',
+                    message: 'What is the employees role?',
+                    choices: roleChoices
+                },
+
+            ]
+        )
+        .then(function (answer) {
+            console.log(answer);
+
+            var query = `INSERT INTO employee SET ?`
+
+            connection.query(query, {
+                first_name: answer.first_name,
+                last_name: answer.last_name,
+                role_id: answer.role_id,
+                manager_id: answer.manager_id,
+            },
+
+                function (err, res) {
+                    if (err) throw err;
+
+                    console.table(res);
+                    console.log(res.insertedRows + 'Inserted successfully!\n');
+
+                    prompt();
+                });
+        });
+}
+
+
+connection.end();
